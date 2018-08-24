bcfc2964884dd9d330a5fcbd054e12e6912ace1c
Ivo Jurisic <i.jurisic@emakina.hr>
2018-08-24 16:33:30 +0200

    polished animations

--- src/Education.js
+++ src/Education.js
@@ -3,34 +3,32 @@ import './Object.css';
 
 const Education = ({ employer, description, descriptionList, image }) => {
     return (
-        <div>
-            <section className="object-container">
-                <div className="container">
-                    <div className="object">
-                        <div className="row">
-                            <div className="col-sm-8">
-                                <h3 className="employer">
-                                    {employer}
-                                </h3>
-                                <div className="description">
-                                    {description}
-                                </div>
-                                <ul className="description-list">
-                                    {descriptionList.map(item => <li>{item}</li>)}
-                                </ul>
+        <section className="object-container">
+            <div className="container">
+                <div className="object">
+                    <div className="row">
+                        <div className="col-sm-8">
+                            <h3 className="employer">
+                                {employer}
+                            </h3>
+                            <div className="description">
+                                {description}
                             </div>
-                            <div className="image-container">
-                                <div className="col-sm-4">
-                                    <img className="image" src={image} alt="logo" />
-                                </div>
+                            <ul className="description-list">
+                                {descriptionList.map(item => <li>{item}</li>)}
+                            </ul>
+                        </div>
+                        <div className="image-container">
+                            <div className="col-sm-4">
+                                <img className="image" src={image} alt="logo" />
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="object__bg" style={{ backgroundImage: `url(${image})` }}>
-                </div>
-            </section>
-        </div>
+            </div>
+            <div className="object__bg" style={{ backgroundImage: `url(${image})` }}>
+            </div>
+        </section>
     );
 }
 
