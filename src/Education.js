8011c70e9bce30512041976fff8df3a726eabf0d
Ivo Jurisic <i.jurisic@emakina.hr>
2018-09-04 09:58:38 +0200

    added proper fade efffect

--- src/Education.js
+++ src/Education.js
@@ -1,33 +1,39 @@
 import React from "react";
+import Fade from "react-reveal/Fade";
+// import Fade from "react-reveal/Fade";
 import "./styles/App.scss";
 
 const Education = ({ employer, description, descriptionList, image }) => {
   return (
     <section className="object__container">
       <div className="container">
-        <div className="object">
-          <div className="row">
-            <div className="col-sm-8">
-              <h3 className="employer">{employer}</h3>
-              <div className="description">{description}</div>
-              <ul className="description-list">
-                {descriptionList.map((item, i) => (
-                  <li key={`Nav ${item}${i}`}>{item}</li>
-                ))}
-              </ul>
-            </div>
-            <div className="col-sm-4">
-              <div className="object__image-container">
-                <img className="object__image" src={image} alt="logo" />
+        <Fade>
+          <div className="object">
+            <div className="row">
+              <div className="col-sm-8">
+                <h3 className="employer">{employer}</h3>
+                <div className="description">{description}</div>
+                <ul className="description-list">
+                  {descriptionList.map((item, i) => (
+                    <li key={`Nav ${item}${i}`}>{item}</li>
+                  ))}
+                </ul>
+              </div>
+              <div className="col-sm-4">
+                <div className="object__image-container">
+                  <img className="object__image" src={image} alt="logo" />
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </Fade>
       </div>
-      <div
-        className="object__bg"
-        style={{ backgroundImage: `url(${image})` }}
-      />
+      <Fade>
+        <div
+          className="object__bg"
+          style={{ backgroundImage: `url(${image})` }}
+        />
+      </Fade>
     </section>
   );
 };
--- src/Jobs.js
+++ src/Jobs.js
@@ -1,11 +1,12 @@
 import React from "react";
-import Fade from 'react-reveal/Fade';
+import Fade from "react-reveal/Fade";
 import "./styles/App.scss";
 
-const Jobs = ({ employer, description, descriptionList, image}) => {
+const Jobs = ({ employer, description, descriptionList, image }) => {
   return (
-      <section className="object__container">
+    <section className="object__container">
         <div className="container">
+        <Fade>
           <div className="object">
             <div className="row">
               <div className="col-sm-8">
@@ -17,23 +18,22 @@ const Jobs = ({ employer, description, descriptionList, image}) => {
                   ))}
                 </ul>
               </div>
-              <div className="col-sm-4"> 
-              <Fade>
+              <div className="col-sm-4">
                 <div className="object__image-container">
                   <img className="object__image" src={image} alt="logo" />
                 </div>
-                </Fade>
               </div>
             </div>
           </div>
+          </Fade>
         </div>
-        <Fade>
+      <Fade>
         <div
           className="object__bg"
           style={{ backgroundImage: `url(${image})` }}
         />
-        </Fade>
-      </section>
+      </Fade>
+    </section>
   );
 };
 
