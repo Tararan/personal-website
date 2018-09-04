e47e3b9446355280164d3e675a8fd9549792f089
Tararan <30859637+Tararan@users.noreply.github.com>
2018-09-04 22:56:57 +0200

    changed text and improved performance

--- src/jobsListContent.js
+++ src/jobsListContent.js
@@ -1,45 +1,20 @@
 export const jobsListContent = [
     {
         id: 1,
-        employer: 'Lorem Ipsum',
+        employer: 'Stars, My Destination',
         description: `
-        Suspendisse nec arcu at turpis hendrerit 
-        faucibus et vitae ante. Etiam at pretium massa. 
-        Nulla eget quam et elit euismod placerat sit amet ac quam. 
-        Integer congue pulvinar ligula ut laoreet. 
-        Donec urna tellus, rhoncus at metus eu, venenatis lacinia ante. 
-        Curabitur nec condimentum turpis. Nunc posuere egestas rhoncus. 
-        Integer hendrerit purus eros, ac suscipit odio congue id. 
-        Nam pretium nibh sed lectus suscipit, vel pulvinar lectus malesuada. 
-        Suspendisse nec arcu at turpis hendrerit 
-        faucibus et vitae ante. Etiam at pretium massa. 
-        Nulla eget quam et elit euismod placerat sit amet ac quam. 
-        Integer congue pulvinar ligula ut laoreet. 
-        Donec urna tellus, rhoncus at metus eu, venenatis lacinia ante. 
-        Curabitur nec condimentum turpis. Nunc posuere egestas rhoncus. 
-        Integer hendrerit purus eros, ac suscipit odio congue id. 
-        Nam pretium nibh sed lectus suscipit, vel pulvinar lectus malesuada. 
-        Suspendisse nec arcu at turpis hendrerit 
-        faucibus et vitae ante. Etiam at pretium massa. 
-        Nulla eget quam et elit euismod placerat sit amet ac quam. 
-        Integer congue pulvinar ligula ut laoreet. 
-        Donec urna tellus, rhoncus at metus eu, venenatis lacinia ante. 
-        Curabitur nec condimentum turpis. Nunc posuere egestas rhoncus. 
-        Integer hendrerit purus eros, ac suscipit odio congue id. 
-        Nam pretium nibh sed lectus suscipit, vel pulvinar lectus malesuada. 
+        Gulliver Foyle: Last remaining survivor of a merchant spaceship, the Nomad. Captured by the "Scientific People" on an asteroid, he is tattooed on his face according to their customs. The tattoos are later removed, but the scars left under his skin become visible when his emotions get out of control.
+        The Presteign: Head of the wealthy Presteign clan, whose interests include a chain of luxury department stores, each managed by an identical "Mr. Presto". Wealthy people like Presteign demonstrate their status by using outmoded methods of transportation and never jaunting if they can avoid it. Presteign holds court in his Star Chamber, an elaborate old-fashioned office equipped with a bar, and staffed by robots. It is designed to disorient visitors and give him the psychological edge.
+        Robin Wednesbury: A Telesend, a one-way telepath who can send thoughts but not receive them. Foyle meets her in "jaunte rehab" while pretending to be someone who has lost the ability to jaunte. She discovers his deception but he kidnaps her and rapes her to intimidate her into silence. Later, as Fourmyle, he recruits her to help him navigate the upper echelons of society. After she discovers who he really is, he offers her the prospect of finding her family, who were refugees on the Vorga.
+        Jisbella ("Jiz") McQueen: Serving five years of "cure" in Gouffre Martel for larceny.
         `,
         descriptionList: [
-            'First' , 
-            'Second' , 
-            'First' , 
-            'Second' , 
-            'First' , 
-            'Second'
+            'Gully Foyle is my name' , 
+            'And Terra is my nation' , 
+            'Deep space is my dwelling place' , 
+            'And death is my destination.'
         ],
-        // image: 'https://image.flaticon.com/icons/svg/145/145813.svg'
-        // image: 'http://www.exalt.design/uploads/media/default/0001/01/thumb_10_default_big.png'
         image: 'https://images.unsplash.com/photo-1518727748038-0fc80a6523e9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=84eb4eeda063ad6049cf4d76bc51f2b0'
-        // image: 'https://images.unsplash.com/photo-1531386450450-969f935bd522?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b0e6037f0a7cd63b48cc1562e348eb3a'
     },
     {
         id: 2,
@@ -59,8 +34,6 @@ export const jobsListContent = [
             'Second' , 
             'Third' , 
         ],
-        // image: 'https://vignette.wikia.nocookie.net/logopedia/images/5/5e/Icloud.png/revision/latest/zoom-crop/width/90/height/55?cb=20151107204136'
-        // image: 'https://images.unsplash.com/photo-1510797215324-95aa89f43c33?ixlib=rb-0.3.5&s=1993d88b0615e6c9e635fd3bcf3d6bb8'
         image: 'https://images.unsplash.com/photo-1535709418-6f14c8e87bb6?ixlib=rb-0.3.5&s=5ae3f410af393fa203731b5712d5ecc0'
     },
     
